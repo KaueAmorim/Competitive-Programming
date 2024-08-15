1c44a4a4c5e24dc7313b3cfdcafbae1ea14bc22a
Kauê de Amorim Silva <kaueamorim@ufpr.br>
2024-08-15 14:56:38 -0300

    Update Cthulhu.cpp

--- Codeforces/Cthulhu.cpp
+++ Codeforces/Cthulhu.cpp
@@ -14,30 +14,23 @@ const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
 const int MAX = 1e2+10;
 vector<vector<int>> grafo(MAX);
-vector<bool> visitado(MAX, 0);
-vector<int> anterior(MAX);
+vector<bool> visitado(MAX, false);
 
-bool dfs_ciclo(int atual){
+void dfs(int atual){
     visitado[atual] = true;
 
     for(auto i : grafo[atual]){
         if(!visitado[i]){
-            anterior[i] = atual;
-            dfs_ciclo(i);
-        }
-        else if(anterior[atual] != i){
-            return true;
+            dfs(i);
         }
     }
-
-    return false;
 }
 
 int main(){ _
 
     int n, m; cin >> n >> m;
     
-    while(m--){
+    for(int i = 0; i < m; i++){
         int x, y; cin >> x >> y;
         x--;
         y--;
@@ -45,18 +38,22 @@ int main(){ _
         grafo[y].push_back(x);
     }
     
-    bool tem_ciclo = false;
-    for(int i = 0; i < n; i++){
-        if(!visitado[i]){
-            if(dfs_ciclo(i)){
-                tem_ciclo = true;
+    if(m != n) cout << "NO" << endl;
+    else{
+
+        dfs(0);
+        
+        bool tem_ciclo = true;
+        for(int i = 0; i < n; i++){
+            if(!visitado[i]){
+                tem_ciclo = false;
                 break;
             }
         }
-    }
 
-    if(tem_ciclo) cout << "FHTAGN!" << endl;
-    else cout << "NO" << endl;
+        if(tem_ciclo) cout << "FHTAGN!" << endl;
+        else cout << "NO" << endl;
+    }
 
     return 0;
 }
